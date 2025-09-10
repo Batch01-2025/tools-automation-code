d9d11c37b34a6e8e2d170d6d692ef9e679beba74
MannuA19 <mannmeet.ahir@gmail.com>
2025-09-10 21:17:51 +0530

    Github Actions

--- infra-create/iam.tf
+++ infra-create/iam.tf
@@ -22,6 +22,10 @@ resource "aws_iam_role" "main" {
 resource "aws_iam_instance_profile" "main" {
   name = "${var.name}-role"
   role = aws_iam_role.main.name
+
+  lifecycle {
+    create_before_destroy = true
+  }
 }
 # resource "aws_iam_policy" "main" {
 #   name        = "${var.name}-role-policy"
