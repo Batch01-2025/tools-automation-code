46ea7cd37aa294ecc75e2e0855b16590b54d6b7a
MannuA19 <mannmeet.ahir@gmail.com>
2025-09-10 20:39:43 +0530

    Github Actions

--- variables.tf
+++ variables.tf
@@ -1,21 +1,27 @@
-variable "tool"{
+variable "tool" {
+  description = "Map of tools with instance details"
+  type = map(object({
+    instance_type = string
+    policy_name   = list(string)
+    ports         = map(number)
+    volume_size   = number
+  }))
+
   default = {
     github-runner = {
       instance_type = "t2.small"
-      policy_name = "AdministratorAccess"
-      ports = {}
-      volume_size = 30
+      policy_name   = ["AdministratorAccess"]
+      ports         = {}
+      volume_size   = 30
     }
+
     vault = {
       instance_type = "t2.small"
-      policy_name = []
+      policy_name   = []
       ports = {
         vault = 8200
       }
-      volume_size = 15
+      volume_size   = 15
     }
   }
-}
-variable volume_size {
-  type = number
 }
